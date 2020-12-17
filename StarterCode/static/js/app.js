8dd9a0dabb47a0efb8a2c0b914fb17ab90064770
Rongjun Ding <68705076+Rongjun-Ding@users.noreply.github.com>
2020-12-17 10:23:38 +0800

    'second'

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,19 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Use D3 to select the table
+var tbody  = d3.select("tbody");
+// Start By Clearing Existing Data
+tbody.html("");
+// Loop Through `data` 
+data.forEach((dataRow) => {
+     // Append Table Row `tr` to the Table Body `tbody`
+     var row = tbody.append("tr");
+     // `Object.values` & `forEach` to Iterate Through Values
+     Object.values(dataRow).forEach((value) => {
+         // Append a Cell to the Row for Each Value
+         var cell = row.append("td");
+         cell.text(value);
+     });
+ })
+}
